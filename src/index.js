bf34d7e7c247000b8e88fe879025e96c6a06e5e3
Vlad <vladyn19981998@gmail.com>
2021-03-15 21:23:09 +0300

    feat: add brackets solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let result = [];
+    for(let i = 0; i < str.length; i++) {
+        let foundArray = bracketsConfig.find((bracket) => {return bracket[0] === str[i] || bracket[1] === str[i]});
+        if(str[i] === foundArray[1]) {
+            if(foundArray[0] === result[result.length-1]) {
+                result.length = result.length-1;
+            } else {
+                result.push(str[i]);
+            }
+        } else {
+            result.push(str[i]);
+        }
+    }
+    return result.length === 0;
 }
